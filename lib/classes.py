3c56e99ea4166d7be39edc59ee77dd488fe933a4
Santo Cariotti <sancn@live.com>
2017-07-30 08:39:04 +0200

    imports are on the top

--- lib/classes.py
+++ lib/classes.py
@@ -1,17 +1,17 @@
 import redis, socket, os
 
-RED = '\033[91m'
-YELLOW = '\033[93m'
-GREY = '\033[90m'
-BLACK = '\033[0m'
-BOLD = '\033[1m'
-
 try:
     from .config import config as co
 except:
     from config import config as co
 
 r = redis.Redis(host=co['host'], port=co['port'], unix_socket_path=co['unix_socket_path'], db=co['db'])
+
+RED = '\033[91m'
+YELLOW = '\033[93m'
+GREY = '\033[90m'
+BLACK = '\033[0m'
+BOLD = '\033[1m'
 
 def clear():
     """
